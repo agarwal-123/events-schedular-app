fefe3285ada3e9e71c34746183139f5a1c7839bd
agarwal-123 <43489651+agarwal-123@users.noreply.github.com>
2020-06-19 01:32:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
+//cors
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
 
 
 app.use(express.json())
